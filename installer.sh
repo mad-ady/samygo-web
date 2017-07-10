2b40e8151e51b48f174c799f8aac2fe2bb278c67
Adrian Popa <adrian.popa.gh@gmail.com>
2017-07-10 15:34:13 +0300

    Cosmetic changes

--- installer.sh
+++ installer.sh
@@ -4,15 +4,15 @@ echo "Installing samygo-web-api cgi component from $DIR to /mnt/var/www/samygo-w
 if [ ! -d "/mnt/var/www/samygo-web-api/" ]; then
     mkdir -p /mnt/var/www/samygo-web-api/
 fi
-cp -av "$DIR/colorbars.jpg" /mnt/var/www/samygo-web-api/
-cp -av "$DIR/cgi-bin/samygo-web-api.cgi" /mnt/var/www/samygo-web-api/
+cp -a "$DIR/colorbars.jpg" /mnt/var/www/samygo-web-api/
+cp -a "$DIR/cgi-bin/samygo-web-api.cgi" /mnt/var/www/samygo-web-api/
 
 echo "Installing cjpeg dependency to /mnt"
-cp -av "$DIR/cjpeg/cjpeg" /mnt/bin/
-cp -av "$DIR/cjpeg/libjpeg.so.62" /mnt/usr/lib/
+cp -a "$DIR/cjpeg/cjpeg" /mnt/bin/
+cp -a "$DIR/cjpeg/libjpeg.so.62" /mnt/usr/lib/
 
 echo "Installing startup script to /mnt/etc/init.d/"
-cp -av "$DIR/init.d/99_99_samygo_web_api.init" /mnt/etc/init.d/
+cp -a "$DIR/init.d/99_99_samygo_web_api.init" /mnt/etc/init.d/
 
 echo -n "Checking if apache is running... ";
 out=`netstat -tpan | grep 1080 | grep httpd | grep LISTEN | wc -l`;
@@ -22,7 +22,7 @@ else
   echo " [ FAIL ]"
 fi
 
-echo .
+echo 
 echo "Installation is complete. Remember, for the web api to function, it needs the following external dependencies:"
 echo -n " * samyGOso: https://forum.samygo.tv/viewtopic.php?t=6186 "
 if [ -x "/mnt/opt/privateer/usr/bin/samyGOso" ]; then
@@ -52,9 +52,9 @@ else
   echo "[MISSING]"
 fi
 
-echo .
+echo 
 echo "To start the web API and generate your key please run:"
 echo "  /mnt/etc/init.d/99_99_samygo_web_api.init start"
-echo .
+echo 
 echo "The web api will be available at http://tv-ip:1080/cgi-bin/samygo-web-api.cgi"
 echo "Consult the API documentation at https://github.com/mad-ady/samygo-web"
